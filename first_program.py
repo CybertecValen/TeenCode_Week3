76fa908e51347dd7334389dcd3865d8f15dbcaf0
CybertecValen <valenwils@gmail.com>
2019-01-23 13:18:25 +0300

    first python program teencode for day one completed

--- first_program.py
+++ first_program.py
@@ -1,17 +1,35 @@
 #A python program Challenege day1 for teencode#
 
+#code for entering year of birth#
+
+print ("Enter The Current Year")
 num1 = int( input("Enter current year: "))
-num2 = int( input("Enter year of birt") )
-num3 = (num1 - num2)
+
+#code for entering current year
+
+print("Enter Your Year Of Birth")
+num2 = int( input("Enter year of birth: ") )
+
+#variables years storing the output of num1 and num2
+
+years: int = num1 - num2
+
+#code if statement for varing years#
+
+if(years<18):
+        print("YOU ARE A " + str(years) + " " "YEARS OLD")
+
+elif( (years>=18) and (years<=36)):
+        print("YOU ARE A " + str(years) + " " "YEARS OLD")
+
+else:
+        print("YOUN ARE A" + str(years) + " " "YEARS OLD")
 
 
-def calc_age(num3):
-        if(num3<18):
-                print(num3 + "You are a Minor!")
-        elif( (num3>=18)and(num3<=36)):
-                print(num3 + "You are a Youth!")
-        else:
-                print(num3 + "You are an elder!")
+
+
+
+
         
 
 
